b42013704dac254b70f7389067d1ac8043030792
Kyle Pekosh <kylep342@gmail.com>
2022-03-05 13:01:45 -0600

    Cleans up function

--- src/js/lib.js
+++ src/js/lib.js
@@ -9,7 +9,11 @@ This library contians funcitons used to in personal financial analysis
 */
 
 exports.amortize = function (principal, annualRate, periodsPerYear, years) {
-    const periodRate = annualRate / periodsPerYear;
-    const numPayments = periodsPerYear * years;
-    return principal * ((periodRate * (1 + periodRate) ** numPayments) / ((1 + periodRate) ** numPayments - 1))
+    return principal * (
+        (
+            (annualRate / periodsPerYear) * (1 + (annualRate / periodsPerYear)) ** (periodsPerYear * years)
+        ) / (
+            (1 + (annualRate / periodsPerYear)) ** (periodsPerYear * years) - 1
+        )
+    );
 }
