b1dbc478a0e47c6c72407da6028c4ba21f2d91c3
Kyle Pekosh <kylep342@gmail.com>
2022-03-16 23:11:46 -0500

    Adds package.json Adds several pieces to lib Adds a Loan class with methods and supporting functions

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -8,12 +8,60 @@ This library contains functions used to in personal financial analysis
 
 */
 
-export function amortize (principal, annualRate, periodsPerYear, years) {
+// export function amortize (principal, annualRate, periodsPerYear, years) {
+function amortize (principal, periodRate, periods) {
     return principal * (
         (
-            (annualRate / periodsPerYear) * (1 + (annualRate / periodsPerYear)) ** (periodsPerYear * years)
+            periodRate * (1 + periodRate) ** periods
         ) / (
-            (1 + (annualRate / periodsPerYear)) ** (periodsPerYear * years) - 1
+            (1 + periodRate) ** periods - 1
         )
     );
+}
+
+// export function principalRemaining (principal, payment, periodRate, periods) {
+function principalRemaining (principal, payment, periodRate, periods) {
+    return (
+        (principal * (1 + periodRate) ** periods) - (
+            payment * (
+                ((1 + periodRate) ** periods - 1) / (periodRate)
+            )
+        )
+    );
+}
+
+class Loan {
+    constructor (principal, annualRate, periodsPerYear, term, pmt=null) {
+        this.principal = principal;
+        this.annualRate = annualRate;
+        this.periodsPerYear = periodsPerYear;
+        this.term = term;
+        this.periodRate = this.annualRate  / this.periodsPerYear;
+        this.periods = this.periodsPerYear * this.term;
+        this.minPmt = this.amortize(this.principal, this.periodRate, this.periods);
+        this.totalInterest = (this.minPmt * (this.periodsPerYear * this.term)) - this.principal;
+        this.pmt = this.validatePayment(this.minPmt, pmt);
+    }
+
+    validatePayment(minPmt, pmt=null) {
+        if (pmt === null) {
+            return minPmt
+        } else if (pmt < minPmt) {
+            throw `pmt cannot be less than ${minPmt}`
+        } else {
+            return pmt
+        }
+    }
+
+    amortize() {
+        return amortize(this.principal, this.periodRate, this.periods);
+    }
+
+    principalRemaining(periods) {
+        return principalRemaining(this.principal, this.pmt, this.periodRate, periods);
+    }
+
+    interestPaid(periods) {
+        return (this.pmt * periods) - (this.principal - principalRemaining(this.principal, this.pmt, this.periodRate, periods))
+    }
 }
