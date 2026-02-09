5e826cc55e8a36a5188766cb729d32e0099e9a92
Kyle Pekosh <kylep342@gmail.com>
2026-02-09 17:38:53 -0600

    Fixes test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -10,55 +10,20 @@ import * as moneyfunx from '../src/index';
 describe('moneyfunx module', () => {
   it('exports expected members matching the new descriptive naming standard', async () => {
     const expectedExports: string[] = [
-      // Constants
-      'MAX_DURATION_YEARS',
-      'TOTALS',
-
-      // Debt / Loans
-      'ILoan',
-      'Loan',
-      'determineExtraPayment',
-      'amortizePayments',
-      'payLoans',
-      'LoansPaymentSchedule',
-      'LoanPrincipals',
-      'PaymentSchedule',
-      'PaymentRecord',
-
-      // Investment / Accumulation
-      'IInstrument',
-      'Instrument',
-      'amortizeContributions',
-      'contributeInstruments',
-      'determineExtraContribution',
-      'ContributionRecord',
-      'ContributionSchedule',
-      'InstrumentsContributionSchedule',
-
-      // Drawdown / Distribution
-      'calculateAmortizedWithdrawal',
-      'drawdownInstruments',
-      'WithdrawalRecord',
-      'WithdrawalSchedule',
-      'InstrumentsWithdrawalSchedule',
-      'performWaterfallDrawdown',
-
-      // Shared Math Primitives
-      'calculatePeriodicAmount',
-      'calculateBalanceRemaining',
-      'calculatePeriodsToZero',
-      'calculateInterestOverPeriods',
-
-      // Errors
-      'NegativeContributionError',
-      'PaymentTooLowError',
-      'NegativeWithdrawalError',
-
-      // Shared Utilities
-      'HasRateAndBalance',
-      'snowball',
-      'avalanche',
-      'sortWith',
+      'constants',
+      'contributionTypes',
+      'contributions',
+      'default',
+      'errors',
+      'instrument',
+      'loan',
+      'paymentTypes',
+      'payments',
+      'primitives',
+      'sorting',
+      'strategies',
+      'withdrawalTypes',
+      'withdrawals',
     ].sort();
 
     expect(Object.keys(moneyfunx).sort()).toStrictEqual(expectedExports);
